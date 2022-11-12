37edc61760d0553b951949fefd85ef90b415a7ea
Yanguk <heo.yanguk@gmail.com>
2022-11-12 11:23:28 +0900

    feature: os모듈 cpu 데이터 가공
    현재 사용량을 percentage로 나타낼수 있게 가공하였습니다

--- src/lib/fp/util.js
+++ src/lib/fp/util.js
@@ -1,4 +1,4 @@
-export const curry = (fn) => (a, ...rest) =>
+export const curry = fn => (a, ...rest) =>
   rest.length ? fn(a, ...rest) : (...rest) => fn(a, ...rest);
 
 export const add = (a, b) => a + b;
@@ -7,4 +7,9 @@ export const subtract = (a, b) => a - b;
 
 export const bValue = curry((key, obj) => obj[key]);
 
-export const identity = (value) => value;
+export const identity = value => value;
+
+export const makePercentage = curry((fixNum, float) =>
+  Math.ceil(float * 10 ** (2 + fixNum)) / 100);
+
+export const log = console.log;
--- src/mainProcess/index.js
+++ src/mainProcess/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
 import { app, BrowserWindow } from 'electron';
 import './os';
